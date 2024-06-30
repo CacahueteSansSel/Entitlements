7f5aadd18d22d67b382050b05c3e476f50e4d2a8
Cacahuète Dev <24205659+CacahueteSansSel@users.noreply.github.com>
2024-06-30 10:20:28 +0200

    only register overlay on client, fixing a crash on server

--- common/src/main/java/dev/cacahuete/entitlements/Entitlements.java
+++ common/src/main/java/dev/cacahuete/entitlements/Entitlements.java
@@ -1,19 +1,21 @@
 package dev.cacahuete.entitlements;
 
 import dev.architectury.event.events.client.ClientGuiEvent;
+import dev.architectury.utils.Env;
+import dev.architectury.utils.EnvExecutor;
 import dev.cacahuete.entitlements.block.BlockRegister;
 import dev.cacahuete.entitlements.block.entity.BlockEntityRegister;
 import dev.cacahuete.entitlements.item.ItemRegister;
 import dev.cacahuete.entitlements.overlay.RegionTitleOverlay;
 import dev.cacahuete.entitlements.tab.CreativeTabRegister;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
 public class Entitlements
 {
 	public static final String MOD_ID = "entitlements";
 
 	public static void init() {
-		ClientGuiEvent.RENDER_HUD.register(RegionTitleOverlay::render);
-
 		CreativeTabRegister.register();
 
 		ItemRegister.register();
@@ -21,5 +23,11 @@ public class Entitlements
 		BlockRegister.registerBlockItemsInto(ItemRegister.ITEMS);
 
 		BlockEntityRegister.register();
+
+		EnvExecutor.runInEnv(Env.CLIENT, () -> Entitlements::initClient);
+	}
+
+	public static void initClient() {
+		ClientGuiEvent.RENDER_HUD.register(RegionTitleOverlay::render);
 	}
 }
